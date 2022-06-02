ea55c5da56070da323c27c1e1d55bfc239642bef
SanjarbekUsmonov <noobpusk@gmail.com>
2022-06-03 01:42:49 +0500

    a href

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,14 +3,28 @@
 
     <q-header class="bg-white text-white row justify-center content-center" style="height: 100px">
       <div class="header  row  justify-between items-start content-center">
-          <div class="logo bg-red" style="width: 20%;height: 100px; ">
-
+          <div class="logo  row content-center" style="width: 20%;height: 100px; ">
+            <q-img src="https://foodsify.xyz/uploads/logo.png"  style="width: 215px;height: 45px; "/>
           </div>
-          <div class="home bg-green" style="width: 45%;height: 100px;">
-
+          <div class="home " style="width: 45%;height: 100px;">
+              <ul class="ul fit row  justify-between items-start content-center">
+                <li> <a href=""></a> Home</li>
+                <li> <a href=""></a> About</li>
+                <li> <a href=""></a> Pricing</li>
+                <li> <a href=""></a> Democ</li>
+                <li> <a href=""></a> Blogs</li>
+                <li> <a href=""></a> Contact</li>
+              </ul>
           </div>
-          <div class="select bg-blue" style="width: 30%;height: 100px;">
-
+          <div class="select " style="width: 30%;height: 100px;">
+              <div class="fit row  justify-end items-start content-center">
+                  <select name="" id="" style="padding: 13px; margin-right: 8px; border-radius: 4px; ">
+                  <option value="eng">English</option>
+                  <option value="rus">Russki</option>
+                  <option value="uzb">Uzbek</option>
+                 </select>
+                 <q-btn color="black" class="btn" padding="13px 35px" label="Become A Partner" />
+              </div>
           </div>
       </div>
     </q-header>
@@ -39,7 +53,21 @@
 .header{
   width: 85%;
   height: 100px;
-  background-color: yellow;
+}
+.ul li{
+ list-style: none;
+ font-size: 16px;
+ margin-bottom: 20px;
+ color: grey;
+}
+.ul li:hover{
+cursor: pointer;
+color: green;
+transition: 0.5s;
+}
+.btn{
+  border-radius: 4px;
+  font-size: 12px;
 }
 
 
